d0e76be009d99320fbb382ec7cb8938c22c77255
Joshua Suskin <jsuskin85@gmail.com>
2018-06-27 03:28:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,5 @@
+const pressedKeys = [];
+
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -12,5 +14,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+document.body.addEventListener('keydown', (e) => {
+
+pressedKeys.push(e.key);
+pressedKeys.splice(-codes.length - 1, pressedKeys.length - codes.length);
+if(pressedKeys.join('').includes(codes)) {
+  alert('You did it!');
+}
+
+});
+
+return pressedKeys;
+
 }
