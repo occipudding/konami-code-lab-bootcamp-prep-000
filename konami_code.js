d7c7906d121246ee372b75653294665ad4eabada
Joshua Suskin <jsuskin85@gmail.com>
2019-04-03 06:28:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,3 @@
-let pressedKeys = [];
-
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -13,29 +11,20 @@ const codes = [
   "a"
 ];
 
-function init() {
-  
-  /*go in the document and give `input` access to <body>*/
-  const input = document.querySelector('body');
-  
-  /*listen to `input` (document.body) for keyboard press & do something when that happens*/
-  input.addEventListener('keydown', (event) => {
-    
-    /*adds key pressed to pressedKeys array*/
-    pressedKeys.push(event.key);
-    
-    /*keeps only the last n keys added to pressedKeys array, where n is the length of codes array*/
-    /*`-codes.length - 1` starts splice at end of pressedKeys array*/
-    /*`pressedKeys.length - codes.length` cuts values from array starting n indexes from end of */
-    /*array, where n is length of `codes` array*/
-    pressedKeys.splice(-codes.length - 1, pressedKeys.length - codes.length);
-  });
-  
-    /*send alert when pressedKeys array holds same values as codes array*/
-    if(pressedKeys == codes) {
-    alert('Yay');
+const body = document.querySelector("body");
+let index = 0;
+
+body.addEventListener('keydown', init);
+
+function init(e) {
+  let key = e.code.includes("Key") ? e.code.slice(e.code.indexOf("y") + 1).toLowerCase() : e.code;  
+  if(key === codes[index]) {
+    index++;
+    if(index === codes.length) {
+      alert("hurray");
+      index = 0;
     }
-
-return pressedKeys;
-
+  } else {
+    index = 0;
+  } 
 }
